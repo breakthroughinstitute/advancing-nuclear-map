d8ecd1c8cadd852ff3cb9c62b8c94d005ef7e2a5
Joy Jiang <138795423+JoyJiang97@users.noreply.github.com>
2026-05-06 15:59:28 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -952,7 +952,7 @@ function swT(t) {
     setTimeout(() => {
       map.invalidateSize();
       // Uprate tab has 300px right panel — zoom out slightly and shift center east
-      if (t === "uprate") map.setView([38, -96], 4.9);
+      if (t === "uprate") map.setView([38, -96], 4.8);
     }, 100);
     if (t === "map") {
       uM();
